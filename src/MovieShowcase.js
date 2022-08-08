763d0e75d817aca89928dfdd3ee86d1f6fa49825
DannnLim <Dannylim-@hotmail.com>
2022-08-08 19:22:56 +1000

    commit tree 327cc61473bcadaaa8b1ff0812c3761ec2cc1287

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,3 +1,4 @@
+import { list } from 'mocha/lib/reporters/base.js';
 import React, { Component } from 'react';
 import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
@@ -6,6 +7,7 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return movieData.map(Movie => <MovieCard title={Movie.title} IMDBRating={Movie.IMDBRating} genres={Movie.genres} poster={Movie.poster} />)
   }
 
   render() {
@@ -14,5 +16,5 @@ export default class MovieShowcase extends Component {
         {this.generateMovieCards()}
       </div>
     )
-  }
+  }                                                                  
 }
